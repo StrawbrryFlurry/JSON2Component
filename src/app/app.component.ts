82bfa1d61204bddc8efa3945b2a1c4b0b4a1033e
Michael <michael.straub@denner.ch>
2020-10-14 20:17:51 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-j2c-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'JSON2Component';
+}
